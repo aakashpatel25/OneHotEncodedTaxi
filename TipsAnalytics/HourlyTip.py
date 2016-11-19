855e5dc86570430ab6e084837c7406f7af9289bc
Ruturaj Patel <ruturajp@sfu.ca>
2016-11-19 15:27:21 -0800

    Changed the hourlytip.py to match the s3 folders

--- TipsAnalytics/HourlyTip.py
+++ TipsAnalytics/HourlyTip.py
@@ -4,13 +4,19 @@ import sys, operator
 from datetime import datetime
 from pyspark.sql.functions import hour, avg
 
-inputs = 'dataparquet/'
 
-myParquet = sqlContext.read.parquet(inputs)
-tipsData = myParquet.select('carId','driverId','pickupTime','paymentType','tripDistance','fareAmount','tipAmount','totalAmount')
 
-pickupDistTip = tipsData.select('pickupTime','tipAmount')
+s3 = 's3://taxidata.com/'
+year1 = s3+'2010/'
+year2 = s3+'2011/'
+year3 = s3+'2012/'
+year4 = s3+'2013/'
+
+myParquet = sqlContext.read.parquet(year1,year2,year3,year4)
+
+pickupDistTip = myParquet.select('pickupTime','tipAmount')
 (pickupDistTip
     .groupBy(hour("pickupTime").alias("hour"))
     .agg(avg("tipAmount").alias("totalTips"))
-    .show(1000))
+    
+averageSalary.write.mode('overwrite').format("com.databricks.spark.csv").save('hourlytips')
