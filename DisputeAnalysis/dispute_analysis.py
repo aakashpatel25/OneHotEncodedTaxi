4483c22deb6dbf63b6d07082b194d555f17f3b3b
Ruturaj Patel <ruturajp@sfu.ca>
2016-11-28 22:44:00 -0800

    Added dispute Analysis for Newyork Taxi based on various criterias

---
+++ DisputeAnalysis/dispute_analysis.py
@@ -0,0 +1,72 @@
+'''
+@author:ruturajp
+@date:28-Nov-2016
+
+Find the disputed trips by month, year.
+Find most disputed trips on an hour
+Find the amount of the trips which are disputed.
+
+'''
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+import sys, operator
+import re, string
+from pyspark.sql.functions import *
+
+conf = SparkConf().setAppName('Payment Type Analysis')
+sc = SparkContext(conf=conf)
+
+sqlContext= SQLContext(sc)
+
+s3 = 's3://taxidata.com/'
+year1 = s3+'2010/'
+year2 = s3+'2011/'
+year3 = s3+'2012/'
+year4 = s3+'2013/'
+
+#parquet file
+myParquet = sqlContext.read.parquet(year1,year2,year3,year4)
+
+#Transaction dataframe
+transDF = myParquet.select(trim(upper(myParquet.paymentType)).alias('paymentType') \
+                    ,year('pickupTime').alias('year') \
+                    ,month('pickupTime').alias('month') \
+                    ,hour('pickupTime').alias('hour') \
+                    ,'totalAmount').where('totalAmount > 0')
+
+#no of disputed trips per hour
+disputed_per_hour = (transDF.filter("paymentType = 'DIS'")
+     .groupBy('hour','paymentType')
+     .agg(count("paymentType").alias("disputedTrips"))
+     .orderBy("hour")
+     .coalesce(1))
+
+#Output to csv
+disputed_per_hour.write.mode('overwrite') \
+                        .format("com.databricks.spark.csv") \
+                        .save('s3://taxidata.com/DisputeAnalysis/DisputePerHour')
+
+#No of Trips with  every month
+disputed_per_month = (transDF.filter("paymentType = 'DIS'")
+     .groupBy('year','month','paymentType')
+     .agg(count("paymentType").alias("tripsCount"))
+     .orderBy("year")
+     .coalesce(1))
+
+#Output to csv
+disputed_per_month.write.mode('overwrite') \
+                        .format("com.databricks.spark.csv") \
+                        .save('s3://taxidata.com/DisputeAnalysis/DisputesPerMonth')
+
+
+#Amount of Money involved in disputed trips
+disputed_amounts = (transDF.filter("paymentType = 'DIS'")
+     .groupBy('year','month')
+     .sum("totalAmount").alias("disputeAmount")
+     .orderBy("year")
+     .coalesce(1))
+
+#Output to csv
+disputed_amounts.write.mode('overwrite') \
+                        .format("com.databricks.spark.csv") \
+                        .save('s3://taxidata.com/DisputeAnalysis/DisputedAmount')
